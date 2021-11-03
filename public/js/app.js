3fd27244447badd1405161933224c0e64ed0a1d0
consoledot <bimbooladele@outlook.com>
2021-11-04 00:39:46 +0100

    added footer and scroll behaviour

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Hallo h");
+// Declare varibales
+var navigation = document.getElementById("navigation");
+var contactButton = document.getElementById("contact-button");
+var fixedBackground = "navigation__fixed-background";
+// Function to add background color to navigation
+window.addEventListener("scroll", function () {
+    window.scrollY > 30
+        ? navigation.classList.add(fixedBackground)
+        : navigation.classList.remove(fixedBackground);
+});
+// Function to scroll to specific id on a page
+var scrollToView = function (ref) {
+    var element = document.getElementById(ref);
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "end" });
+};
+contactButton.addEventListener("click", function () {
+    scrollToView("section-contact");
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("Hallo h");
+// Declare varibales
+const navigation: HTMLElement | any = document.getElementById("navigation");
+const contactButton: HTMLElement | any =
+  document.getElementById("contact-button");
+const fixedBackground: string = "navigation__fixed-background";
+
+// Function to add background color to navigation
+window.addEventListener("scroll", (): void => {
+  window.scrollY > 30
+    ? navigation.classList.add(fixedBackground)
+    : navigation.classList.remove(fixedBackground);
+});
+
+// Function to scroll to specific id on a page
+const scrollToView = (ref: string): void => {
+  const element: HTMLElement | any = document.getElementById(ref);
+  element.scrollIntoView({ behavior: "smooth", block: "start", inline: "end" });
+};
+
+contactButton.addEventListener("click", (): void => {
+  scrollToView("section-contact");
+});
