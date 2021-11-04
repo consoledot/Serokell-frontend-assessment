52a9730b4c54439cb34f67972e6d160de0c71fc9
consoledot <bimbooladele@outlook.com>
2021-11-04 11:08:56 +0100

    added menu, dropdowns and some changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 // Declare varibales
 var navigation = document.getElementById("navigation");
 var contactButton = document.getElementById("contact-button");
+var menu = document.getElementById("menu");
+var dropdownList = document.getElementById("dropdown-list");
 var fixedBackground = "navigation__fixed-background";
 // Function to add background color to navigation
 window.addEventListener("scroll", function () {
@@ -17,3 +19,10 @@ var scrollToView = function (ref) {
 contactButton.addEventListener("click", function () {
     scrollToView("section-contact");
 });
+// Function to show dropdown
+var ToggleShow = function () {
+    navigation.classList.toggle("show");
+    menu.classList.toggle("show");
+    dropdownList.classList.toggle("show");
+};
+menu.addEventListener("click", ToggleShow);
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@
 const navigation: HTMLElement | any = document.getElementById("navigation");
 const contactButton: HTMLElement | any =
   document.getElementById("contact-button");
+const menu: HTMLElement | any = document.getElementById("menu");
+const dropdownList: HTMLElement | any =
+  document.getElementById("dropdown-list");
 const fixedBackground: string = "navigation__fixed-background";
 
 // Function to add background color to navigation
@@ -20,3 +23,12 @@ const scrollToView = (ref: string): void => {
 contactButton.addEventListener("click", (): void => {
   scrollToView("section-contact");
 });
+
+// Function to show dropdown
+const ToggleShow = (): void => {
+  navigation.classList.toggle("show");
+  menu.classList.toggle("show");
+  dropdownList.classList.toggle("show");
+};
+
+menu.addEventListener("click", ToggleShow);
